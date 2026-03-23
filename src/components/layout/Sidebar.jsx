c1180fac14d9f5bef0b1fb9373d184bb7eb4d5c4
Balla221 <balla.beye_220540@groupeiam.com>
2026-03-23 18:23:41 +0000

    Au niveau du sidebar faire les donnees pour que ca soit dynamique

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { LOGO } from "../../data/dashboardData";
+import LOGO from "../../assets/images/logo.jpeg";
+
 import { 
   nombreClientsActifs,
   nombreVendeursActifs,
